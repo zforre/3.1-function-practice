29ecb2470ddcbbc0f8e067ce238200ff212d2649
Mady Chapman <MC3D@users.noreply.github.com>
2019-09-11 11:36:22 -0400

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -55,63 +55,6 @@
 
 
 
-(function() {
-  'use strict';
-
-  // ---------------------
-  // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
-  // ---------------------
-
-
-
 
 
   // ---------------------
@@ -124,29 +67,6 @@
 
   // ---------------------
   // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
-  // ---------------------
-
-
-
-  
-
-})();
-
-
-  // ---------------------
-  // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
-  // ---------------------
-
-
-
-
-
-  // ---------------------
-  // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
-  // ---------------------
-
-
-
-  
+  // --------------------- 
 
 })();
